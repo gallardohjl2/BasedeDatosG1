76ee72f45a301f18e199cf40cd4adc7fe6a147b5
Jose Luis Gallardo <gallardohjl2@gmail.com>
2025-05-21 07:56:29 -0600

    Logro: Inicio de repositorio de Base de Datos

---
+++ 01-Fundamentos-de-basededatos/01-query-sql-ldd-lmd-mssql.sql
@@ -0,0 +1,44 @@
+-- SQL-LDD
+-- Crea una base de datos
+CREATE DATABASE bdpruebag1;
+-- Cambiar de base de datos
+USE bdpruebag1;
+-- Crear una tabla 
+CREATE TABLE categoria(
+  idcategoria int not null identity(1,1),
+  categorianombre nvarchar(20) not null,
+  constraint pk_categoria
+  primary key(idcategoria)
+);
+
+CREATE TABLE producto(
+ idproducto int not null, 
+ nombreproducto varchar(20) not null, 
+ descripcion varchar(100) null, 
+ existencia int not null, 
+ precio money not null, 
+ idcategoria int not null,
+ constraint pk_producto
+ primary key(idproducto),
+ constraint fk_producto_categoria
+ foreign key(idcategoria)
+ references categoria(idcategoria)
+);
+
+-- SQL-LMD
+-- Insertar en la tabla categoria
+
+insert into categoria (categorianombre)
+values('Vinos y Licores');
+
+insert into categoria (categorianombre)
+values('Carnes Frias'),
+      ('Linea Blanca'),
+	  ('Lacteos');
+
+update categoria
+set categorianombre = 'Carnes Re-Frias'
+where idcategoria = 2;
+
+select * from categoria;
+select categorianombre  from categoria;
---
+++ README.md
@@ -0,0 +1,12 @@
+# Base de Datos 
+
+> En este repositorio se encuentran temas en relación a base de datos, diseño, lenguaje sql, manejo de Sistemas Gestores de Base de datos como Mysql y Microsoft SQL Server
+
+Los temas que se encuentran en el repositorio son:
+
+1. Fundamentos de Base de datos.
+2. Modelado de Base de Datos
+3. Construcción de Base de Datos
+4. Lenguaje Estructurado de Consulta
+
+![Logo Base de Datos](./img/que-es-una-base-de-datos.png)
